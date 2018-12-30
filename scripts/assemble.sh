fe458d7e94a4fd01b07b8d31b7bcaeb50879e152
Artur Dryomov <artur.dryomov@gmail.com>
2018-12-30 18:50:14 +0300

    Move scripts to a separate directory.

---
+++ scripts/assemble.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+HUGO_VERSION="0.52"
+HUGO_PACKAGE="hugo.tar.gz"
+HUGO_DIRECTORY="hugo"
+HUGO_BINARY="hugo"
+
+
+curl \
+  --location "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz" \
+  --output "${HUGO_PACKAGE}"
+
+mkdir -p "${HUGO_DIRECTORY}" && tar -xzf "${HUGO_PACKAGE}" -C "${HUGO_DIRECTORY}"
+
+
+"./${HUGO_DIRECTORY}/${HUGO_BINARY}"
---
+++ scripts/publish.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+HUGO_DIRECTORY="public"
+PUBLISH_BRANCH="gh-pages"
+
+if [[ $(git status --short) ]]; then
+  echo "Please commit pending changes."
+  exit 1
+fi
+
+echo ":: Removing [${HUGO_DIRECTORY}]."
+rm -rf "${HUGO_DIRECTORY}"
+
+echo ":: Removing [${HUGO_DIRECTORY}] working tree."
+git worktree remove "${HUGO_DIRECTORY}"
+
+echo ":: Making [${HUGO_DIRECTORY}] working tree."
+git worktree add -B "${PUBLISH_BRANCH}" "${HUGO_DIRECTORY}" origin/"${PUBLISH_BRANCH}"
+
+echo ":: Removing [${HUGO_DIRECTORY}] working tree contents."
+rm -rf "${HUGO_DIRECTORY}"/*
+
+echo ":: Generating..."
+hugo
+
+echo ":: Committing generated contents."
+cd "${HUGO_DIRECTORY}"
+git add --all
+git commit --message "Publish Hugo-generated contents."
+
+echo ":: Pushing..."
+#git push origin "${PUBLISH_BRANCH}"
