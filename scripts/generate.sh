e3ae470bf563262c072f2f5818c3b817d421c76f
Artur Dryomov <artur.dryomov@gmail.com>
2026-08-18 09:11:24 +0200

    Change generation script to use temporary files

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,10 +3,10 @@ set -eou pipefail
 
 readonly HUGO_VERSION="0.165.0"
 
-readonly HUGO_PACKAGE_NAME="hugo.tar.gz"
-readonly HUGO_PACKAGE_PATH="hugo"
+HUGO_PATH="$(mktemp --directory)"; readonly HUGO_PATH
+HUGO_PACKAGE_PATH="${HUGO_PATH}/hugo.tar.gz"; readonly HUGO_PACKAGE_PATH
 
-trap 'rm -rf "${HUGO_PACKAGE_NAME}" "${HUGO_PACKAGE_PATH}"' EXIT
+trap 'rm -rf "${HUGO_PATH}"' EXIT
 
 echo ":: Fetching Hugo..."
 
@@ -16,13 +16,12 @@ curl \
   --show-error \
   --retry 3 \
   --location "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux-amd64.tar.gz" \
-  --output "${HUGO_PACKAGE_NAME}"
+  --output "${HUGO_PACKAGE_PATH}"
 
-echo ":: Unpacking Hugo..."
+echo ":: Extracting Hugo..."
 
-mkdir --parents "${HUGO_PACKAGE_PATH}"
-tar --extract --gzip --file "${HUGO_PACKAGE_NAME}" --directory "${HUGO_PACKAGE_PATH}"
+tar --extract --gzip --file "${HUGO_PACKAGE_PATH}" --directory "${HUGO_PATH}"
 
 echo ":: Running Hugo..."
 
-"${HUGO_PACKAGE_PATH}/hugo"
+"${HUGO_PATH}/hugo"
